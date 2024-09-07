cdf37c9fa693900574991a78bad3edf3734f8a7e
odan <odan3240@gmail.com>
2024-09-07 17:00:22 +0900

    add panda css

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import styles from "./index.css?url";
+import type { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
